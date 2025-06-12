1fa430a4249e40be2785c539eb9326ab3a7183de
Olihdezp <oligamer112@gmail.com>
2025-06-12 19:34:18 +0100

    Fix

--- src/net/salesianos/mesa/Table.java
+++ src/net/salesianos/mesa/Table.java
@@ -2,11 +2,11 @@ package net.salesianos.mesa;
 
 import java.io.Serializable;
 
-public class Mesa implements Serializable {
+public class Table implements Serializable { // serializable para que se guarde en bytes el progreso
     private String color;
     private int numLegs;
 
-    public Mesa(String color, int numLegs) {
+    public Table(String color, int numLegs) {
         this.color = color;
         this.numLegs = numLegs;
     }
