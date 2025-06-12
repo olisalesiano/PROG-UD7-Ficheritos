b7c06a47808b73f49c9b80c936db0bfb0ee7368b
Olihdezp <oligamer112@gmail.com>
2025-06-12 18:47:13 +0100

    Fix: Added functions to 'MergeFiles.java'
    añadida las funciones a 'MergeFiles.java' para que lo que se escriba en los archivos 'act2' y 'userInput' se transcriban al fichero 'file1and2'
    
    profe estoy usando el traductor, yo no uso al señor gpt 😛

--- src/net/salesianos/save_text/MergeFiles.java
+++ src/net/salesianos/save_text/MergeFiles.java
@@ -3,6 +3,7 @@ package net.salesianos.save_text;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class MergeFiles {
@@ -12,7 +13,37 @@ public class MergeFiles {
         File file = new File("files/file1and2");
 
         try {
-            BufferedReader reader1 = new BufferedReader(new FileReader("files/file1and2"));
+            // ? lee archivo 1 ('userInput')
+            BufferedReader reader1 = new BufferedReader(new FileReader("files/userInput"));
+            String line1 = reader1.readLine(); // si está vacío devuelve null (cierra while)
+            String fulltext1 = "";
+
+            while (line1 != null) {
+                fulltext1 += line1 + " ";
+                line1 = reader1.readLine(); // lee la siguiente línea y si no, da null
+            }
+            // System.out.println(fulltext1);
+            reader1.close();
+
+            // ? lee archivo 2 ('act2')
+            BufferedReader reader2 = new BufferedReader(new FileReader("files/act2"));
+            String line2 = reader2.readLine();
+            String fulltext2 = "";
+            while (line2 != null) {
+                fulltext2 += line2 + " ";
+                line2 = reader2.readLine();
+            }
+            // System.out.println(fulltext2);
+            reader2.close();
+
+            // ? re write el nuevo ficherito
+            FileWriter writer = new FileWriter("files/file1and2");
+            writer.write("- Contenido del Fichero Uno: <" + fulltext1 + ">");
+            writer.write("- Contenido del Fichero Dos: <" + fulltext2 + ">");
+            writer.write("== FICHERO CREADO POR OLI ==");
+            writer.flush();
+            writer.close();
+
         } catch (IOException exception) {
             System.out.println("No se pudo leer el archivo mister.");
         }
