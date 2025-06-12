101b539be6617ba3ce6d6fa0f1ac780fbaab6188
Olihdezp <oligamer112@gmail.com>
2025-06-12 10:35:48 +0100

    Feat: Created new file 'SaveInputText.java'
    Added a new class that passes the user's written text to the 'userInput' file.

--- src/net/salesianos/save_text/SaveInputText.java
+++ src/net/salesianos/save_text/SaveInputText.java
@@ -1,5 +1,41 @@
 package net.salesianos.save_text;
 
-public class SaveInputText {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
+public class SaveInputText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (true) {
+            System.out.println("Introduce un texto de mínimo 30 palabras: ");
+            userInput = scanner.nextLine();
+
+            if (userInput.length() >= 30) {
+                break;
+            }
+            scanner.close();
+        }
+
+        String convertText = userInput.toUpperCase().replace(" ", "_");
+
+        try {
+            // método para escribir el fichero
+            OutputStreamWriter writer = new OutputStreamWriter(
+                    new FileOutputStream("./files/userInput"),
+                    StandardCharsets.UTF_8);
+
+            writer.write(convertText);
+            writer.flush();
+            writer.close();
+            System.out.println("Texto guardado correctamente en 'files/userInput'");
+
+        } catch (IOException e) {
+            System.out.println("Error guardando el texto.");
+        }
+    }
 }
